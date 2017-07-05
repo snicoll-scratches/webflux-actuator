0175b383fd0e883243af68f4b9930514c12995a2
Stephane Nicoll <snicoll@pivotal.io>
2017-07-05 14:38:25 +0100

    Initial import

---
+++ src/main/java/com/example/webflux/actuator/WebfluxActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.example.webflux.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxActuatorApplication.class, args);
+	}
+}
